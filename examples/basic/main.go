e2f1c67cef664ca90930b1525cb182a437bbaaee
“wolf584520” <wolf584520@proton.me>
2023-12-15 16:44:03 +0800

    .

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,12 +1,71 @@
 package main
 
-import webview "github.com/webview/webview_go"
+import (
+	"fmt"
+	webview "github.com/webview/webview_go"
+	"time"
+)
+
+const tagA = `function interceptClickEvent(e) {
+    let href = '';
+    const target = e.target || e.srcElement;
+    console.log(target,target.tagName);
+    if (target.tagName === 'A') {
+        href = target.getAttribute('href');
+    }
+    else if(target.tagName === 'IMG') {
+        href = target.parentElement.getAttribute('href');
+    }
+    if(href) {
+		window.navigate(href);
+        //window.location.href = href;
+        //console.log(href);
+        e.preventDefault();
+		return false;
+    }
+}
+
+if (document.addEventListener) {
+    document.addEventListener('click', interceptClickEvent);
+} else if (document.attachEvent) {
+    document.attachEvent('onclick', interceptClickEvent);
+}`
 
 func main() {
-	w := webview.New(false)
+	w := webview.New(true)
+	w.Navigate("https://buyin.jinritemai.com/dashboard/live/control")
+	w.Init(tagA)
+	//w.Navigate("https://baidu.com")
+	//w.Navigate("https://github.com/webview/webview/blob/master/webview.h")
+	//w.Window()
+	//w.Eval(tagA)
 	defer w.Destroy()
-	w.SetTitle("Basic Example")
-	w.SetSize(480, 320, webview.HintNone)
-	w.SetHtml("Thanks for using webview!")
+	err := w.Bind("getCookie", func(cookie string) {
+		fmt.Println(cookie)
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = w.Bind("open", func(url string) {
+		w.Eval("window.location.href='" + url + "'")
+		fmt.Println("window.location.href='" + url + "'")
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = w.Bind("navigate", func(url string) {
+		fmt.Println("navigate", url)
+		w.Navigate(url)
+	})
+	//w.SetTitle("Basic Example")
+	w.SetSize(480*2, 320*2, webview.HintNone)
+	go func() {
+		time.Sleep(time.Second * 4)
+		//w.Eval(tagA)
+		//w.SetHtml("Thanks for using webview!")
+		//w.Eval("window.open('http://www.baidu.com')")
+		//w.Eval("window.open('https://github.com/webview/webview')")
+	}()
+	//w.SetHtml("Thanks for using webview!")
 	w.Run()
 }
--- examples/bind/main.go
+++ examples/bind/main.go
@@ -1,15 +1,20 @@
 package main
 
-import webview "github.com/webview/webview_go"
+import (
+	"fmt"
+	webview "github.com/webview/webview_go"
+	"time"
+)
 
 const html = `<button id="increment">Tap me</button>
 <div>You tapped <span id="count">0</span> time(s).</div>
+<a href="https://blog.csdn.net/gisdtcn/article/details/131409006" target="_blank">webview</a>
 <script>
   const [incrementElement, countElement] =
     document.querySelectorAll("#increment, #count");
   document.addEventListener("DOMContentLoaded", () => {
     incrementElement.addEventListener("click", () => {
-      window.increment().then(result => {
+      window.increment(countElement.textContent).then(result => {
         countElement.textContent = result.count;
       });
     });
@@ -22,16 +27,50 @@ type IncrementResult struct {
 
 func main() {
 	var count uint = 0
-	w := webview.New(false)
+	w := webview.New(true)
+	w.Init(``)
 	defer w.Destroy()
 	w.SetTitle("Bind Example")
 	w.SetSize(480, 320, webview.HintNone)
 
 	// A binding that increments a value and immediately returns the new value.
-	w.Bind("increment", func() IncrementResult {
+	w.Bind("increment", func(abs string) IncrementResult {
+		fmt.Println(count)
 		count++
+		w.Eval("console.log('increment')")
 		return IncrementResult{Count: count}
 	})
+	w.Bind("open", func(url string) {
+		w.Eval("window.location.href='" + url + "'")
+	})
+	go func() {
+		time.Sleep(time.Second * 3)
+		//		w.Eval(`
+		//function interceptClickEvent(e) {
+		//    let href = '';
+		//    const target = e.target || e.srcElement;
+		//    console.log(target,target.tagName);
+		//    if (target.tagName === 'A') {
+		//        href = target.getAttribute('href');
+		//    }
+		//    else if(target.tagName === 'IMG') {
+		//        href = target.parentElement.getAttribute('href');
+		//    }
+		//    if(href) {
+		//        window.location.href = href;
+		//        console.log(href);
+		//        e.preventDefault();
+		//    }
+		//}
+		//
+		//if (document.addEventListener) {
+		//    document.addEventListener('click', interceptClickEvent);
+		//} else if (document.attachEvent) {
+		//    document.attachEvent('onclick', interceptClickEvent);
+		//}
+		//`)
+		//w.Eval("window.increment('sdfsdfds')")
+	}()
 
 	w.SetHtml(html)
 	w.Run()
